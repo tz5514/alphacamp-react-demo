82f06fa4cd574c856643eb70fdd34441be14eca7
tz5514 <tz5514@gmail.com>
2017-04-18 07:50:36 +0800

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const webpack = require('webpack');
 const webpackCompiler = webpack(require('./webpack.config.js'));
-const port = 3000;
+const port = 3001;
 
 const app = express();
 
